b6c199b1bfc279e5c6d1ea921ec19b058817dcea
ben yan <yanbzybzy2@gmail.com>
2019-11-24 00:45:18 +0000

    publish the sample Blazor website

---
+++ BenTest.js
@@ -0,0 +1,24 @@
+﻿
+function printAlert() {
+    alert("script executed");
+    return "Ben";
+}
+
+function printAlert(message) {
+    alert("script executed: "+ message);
+    return "Ben";
+}
+
+function drawD3() {
+
+    d3.select("svg").append("circle")
+        .attr("r", 5)
+        .attr("cx", 50)
+        .attr("cy", 50)
+        .attr("fill", "red");
+        
+}
+
+d3.select("app").append("span")
+    .text("Hello, world!");
+
